b3fcca4f5a3a546e2dcdf90d42ea408b3cbfd39e
djesusnet13 <djesus.net@gmail.com>
2023-09-24 12:45:55 -0300

    Subida de projeto

---
+++ AutoMapperBenchmark/Dtos/ProductDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AutoMapperBenchmark.Dtos;
+
+public class ProductDto
+{
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+}
